a4500f2a72a33d95e89bd864fc086d1ad50682d9
Mina Essam <mina.essam.kamal@gmail.com>
2020-08-17 13:00:17 +0200

    Update RCC_private.h

--- include/RCC/RCC_private.h
+++ include/RCC/RCC_private.h
@@ -11,8 +11,8 @@
 #define RCC_CR 				*((volatile u32*) 0x40021000)
 #define RCC_CFGR  			*((volatile u32*) 0x40021004)
 #define RCC_CIR				*((volatile u32*) 0x40021008)
-#define RCC_APB2RSTR		*((volatile u32*) 0x4002100C)
-#define RCC_APB1RSTR		*((volatile u32*) 0x40021010)
+#define RCC_APB2RSTR		  *((volatile u32*) 0x4002100C)
+#define RCC_APB1RSTR		  *((volatile u32*) 0x40021010)
 #define RCC_AHBENR			*((volatile u32*) 0x40021014)
 #define RCC_APB2ENR			*((volatile u32*) 0x40021018)
 #define RCC_APB1ENR			*((volatile u32*) 0x4002101C)
@@ -21,16 +21,16 @@
 
 
 /* Clock Types */
-#define RCC_HSE_CRYSTAL 		0
-#define RCC_HSE_RC				1
-#define RCC_HSI					2
-#define RCC_PLL					3
+#define RCC_HSE_CRYSTAL 		    0
+#define RCC_HSE_RC				      1
+#define RCC_HSI					        2
+#define RCC_PLL					        3
 
 /* PLL Options */
 
-#define RCC_PLL_IN_HSI_DIV_2 	0
-#define RCC_PLL_IN_HSE_DIV_2 	1
-#define RCC_PLL_IN_HSE		 	2
+#define RCC_PLL_IN_HSI_DIV_2 	    0
+#define RCC_PLL_IN_HSE_DIV_2 	    1
+#define RCC_PLL_IN_HSE		 	      2
 
 /* PLL Multiplier */
 #define RCC_PLLMUL_X2			0
