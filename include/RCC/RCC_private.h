97f4a25285b18008661f638a711c94e4d71d3b1a
Mina Essam <mina.essam.kamal@gmail.com>
2020-08-17 13:05:46 +0200

    Update RCC_private.h

--- include/RCC/RCC_private.h
+++ include/RCC/RCC_private.h
@@ -6,7 +6,7 @@
 #ifndef RCC_PRIVATE_H
 #define RCC_PRIVATE_H
 
-/*   Register Definition*/
+        /*   Register Definition  */
 
 #define RCC_CR 				      *((volatile u32*) 0x40021000)
 #define RCC_CFGR  			    *((volatile u32*) 0x40021004)
@@ -20,19 +20,19 @@
 #define RCC_CSR				      *((volatile u32*) 0x40021024)
 
 
-/* Clock Types */
+      /* Clock Types */
 #define RCC_HSE_CRYSTAL 		    0
 #define RCC_HSE_RC				      1
 #define RCC_HSI					        2
 #define RCC_PLL					        3
 
-/* PLL Options */
+      /* PLL Options */
 
 #define RCC_PLL_IN_HSI_DIV_2 	    0
 #define RCC_PLL_IN_HSE_DIV_2 	    1
 #define RCC_PLL_IN_HSE		 	      2
 
-/* PLL Multiplier */
+    /* PLL Multiplier */
 #define RCC_PLLMUL_X2			0
 #define RCC_PLLMUL_X3			1
 #define RCC_PLLMUL_X4			2
