db0accb2b355e483af1a1c17106bc19ce93678d6
Mina Essam <mina.essam.kamal@gmail.com>
2020-08-17 12:52:09 +0200

    Create BIT_MATH.h

---
+++ include/LIB/BIT_MATH/BIT_MATH.h
@@ -0,0 +1,13 @@
+/****************************************************************/
+/* Author	: Mina Essam										*/
+/* Date		: 29 JUL 2020										*/
+/* Verison	: V01												*/
+/****************************************************************/
+#ifndef BIT_MATH_H
+#define BIT_MATH_H
+#define SET_BIT(VAR, BIT)			VAR |= (1 << (BIT)) 
+#define CLR_BIT(VAR, BIT)			VAR &= ~(1 << (BIT))
+#define GET_BIT(VAR, BIT)			((VAR >> BIT) &1)
+#define TOG_BIT(VAR, BIT)			VAR ^= (1 << (BIT))
+
+#endif
