1841580605947c96ca6420dc3c84847a68162b1d
Mina Essam <mina.essam.kamal@gmail.com>
2020-08-27 08:27:02 +0200

    Create STK_config.h

---
+++ include/SYSTick_Timer/STK_config.h
@@ -0,0 +1,15 @@
+/*****************************************/
+/* Author  :  Mina Essam                 */
+/* Version :  V01                        */
+/* Date    : 26 August 2020              */
+/*****************************************/
+#ifndef STK_CONFIG_H_
+#define STK_CONFIG_H_
+/* Options: AHB/8
+ * 			AHB
+ */
+#define Clock_Source 	Clock_Source_AHB_8
+#define Clear_Register		0x00
+
+
+#endif /* STK_CONFIG_H_ */
